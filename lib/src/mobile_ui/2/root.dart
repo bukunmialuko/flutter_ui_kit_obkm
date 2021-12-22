787201ef2d58696ec4afe4417da8fdb8ddf68cad
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2021-12-22 16:36:21 +0100

    Structuring folders

---
+++ lib/src/mobile_ui/2/root.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/src/web_ui/1/root.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Page1 extends StatelessWidget {
+  const Page1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
