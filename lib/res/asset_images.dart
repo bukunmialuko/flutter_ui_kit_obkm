35e517d3ded40664893bd6a8e1d0b18df937f9f4
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2021-12-28 15:44:44 +0100

    Working on screen 4

--- lib/res/asset_images.dart
+++ lib/res/asset_images.dart
@@ -1,3 +1,8 @@
 class AssetResources {
   static const PAUSE = "assets/svg/pause.svg";
+  static const PLAY = "assets/svg/play.svg";
+  static const MENU = "assets/svg/menu.svg";
+  static const BACK = "assets/svg/back.svg";
+  static const LADY_PG4 = "assets/png/lady_pg4.png";
+  static const MORE = "assets/svg/more.svg";
 }
