2506c913e743486d6d734898145e74fad8f2db4a
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2021-12-28 17:09:35 +0100

    Worker on page 5

---
+++ lib/src/mobile_ui/5/page5.dart
@@ -0,0 +1,259 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter_ui_kit_obkm/res/asset_images.dart';
+import 'package:flutter_ui_kit_obkm/src/res/colors.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Page5 extends StatefulWidget {
+  const Page5({Key? key}) : super(key: key);
+
+  @override
+  _Page5State createState() => _Page5State();
+}
+
+class _Page5State extends State<Page5> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(
+              parent: AlwaysScrollableScrollPhysics()),
+          child: Padding(
+            padding: EdgeInsets.only(left: 24.w, right: 24.w, bottom: 8.h),
+            child: Column(
+              children: [
+                SizedBox(height: 37.h),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      width: 24.r,
+                      height: 24.r,
+                      child: IconButton(
+                        visualDensity: VisualDensity.adaptivePlatformDensity,
+                        padding: EdgeInsets.zero,
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.add_circle,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "Popular",
+                      style: GoogleFonts.workSans(
+                        textStyle: TextStyle(
+                          fontSize: 20.sp,
+                          color: Colors.black,
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: SizedBox(
+                        width: 24.r,
+                        height: 24.r,
+                        child: IconButton(
+                          visualDensity: VisualDensity.adaptivePlatformDensity,
+                          padding: EdgeInsets.zero,
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.add_circle,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 37.h),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(16.r),
+                  child: Container(
+                    width: 343.w,
+                    height: 170.h,
+                    color: AppColors.grey,
+                    child: Stack(
+                      children: [],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30.h),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Nearby Therapists",
+                      style: GoogleFonts.workSans(
+                        textStyle: TextStyle(
+                          fontSize: 16.sp,
+                          color: Colors.black,
+                          fontStyle: FontStyle.normal,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "See more",
+                        style: GoogleFonts.workSans(
+                          textStyle: TextStyle(
+                            fontSize: 14.sp,
+                            color: Colors.black,
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                SizedBox(height: 30.h),
+                ListView.separated(
+                  shrinkWrap: true,
+                  itemCount: 7,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemBuilder: (c, i) {
+                    return SizedBox(
+                      height: 125.h,
+                      // padding: EdgeInsets.only(left: 11.w),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        // crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Center(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(8.r),
+                              child: Container(
+                                width: 125.h,
+                                height: 125.h,
+                                // margin: EdgeInsets.only(right: 14.w),
+                                color: const Color(0xff525252),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Expanded(
+                                  child: Padding(
+                                    padding: EdgeInsets.only(
+                                      left: 15.w,
+                                      right: 2.w,
+                                      top: 10.h,
+                                      bottom: 10.h,
+                                    ),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Flexible(
+                                          child: Text(
+                                            "Dr. John Doe",
+                                            style: GoogleFonts.workSans(
+                                              textStyle: TextStyle(
+                                                fontSize: 16.sp,
+                                                color: Colors.black,
+                                                fontStyle: FontStyle.normal,
+                                                fontWeight: FontWeight.w600,
+                                              ),
+                                            ),
+                                            maxLines: 3,
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                        ),
+                                        SizedBox(
+                                          height: 8.h,
+                                        ),
+                                        Text(
+                                          "CUHK Medical Centre",
+                                          style: GoogleFonts.workSans(
+                                            textStyle: TextStyle(
+                                              fontSize: 14.sp,
+                                              color: Colors.black,
+                                              fontStyle: FontStyle.normal,
+                                              fontWeight: FontWeight.w400,
+                                            ),
+                                          ),
+                                          maxLines: 1,
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                        SizedBox(
+                                          height: 8.h,
+                                        ),
+                                        Row(
+                                          children: [
+                                            const Icon(
+                                              Icons.star,
+                                              color: Colors.black,
+                                            ),
+                                            Text(
+                                              "4.8",
+                                              style: GoogleFonts.workSans(
+                                                textStyle: TextStyle(
+                                                  fontSize: 14.sp,
+                                                  color: Colors.black,
+                                                  fontStyle: FontStyle.normal,
+                                                  fontWeight: FontWeight.w600,
+                                                ),
+                                              ),
+                                              maxLines: 1,
+                                              overflow: TextOverflow.ellipsis,
+                                            ),
+                                            SizedBox(
+                                              width: 16.w,
+                                            ),
+                                            Flexible(
+                                              child: Text(
+                                                "(156 reviews)",
+                                                style: GoogleFonts.workSans(
+                                                  textStyle: TextStyle(
+                                                    fontSize: 14.sp,
+                                                    color: Colors.black,
+                                                    fontStyle: FontStyle.normal,
+                                                    fontWeight: FontWeight.w600,
+                                                  ),
+                                                ),
+                                                maxLines: 1,
+                                                overflow: TextOverflow.ellipsis,
+                                              ),
+                                            ),
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  },
+                  separatorBuilder: (c, i) {
+                    return SizedBox(height: 24.h);
+                  },
+                ),
+                SizedBox(height: 8.h),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
