03e5ffba981d57aa6244fc00d6ea1edbf92b39cf
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2022-01-25 14:03:29 +0100

    Worked on service locator

--- lib/src/mobile_ui/root/root.dart
+++ lib/src/mobile_ui/root/root.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_ui_kit_obkm/src/mobile_ui/10/page_10.dart';
-import 'package:flutter_ui_kit_obkm/src/mobile_ui/11/page_11.dart';
-import 'package:flutter_ui_kit_obkm/src/mobile_ui/12/page_12.dart';
-import 'package:flutter_ui_kit_obkm/src/mobile_ui/13/page_13.dart';
+import 'package:flutter_ui_kit_obkm/src/mobile_ui/14/page_14.dart';
 
 class RootWidget extends StatelessWidget {
   const RootWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Page13();
+    return const Page14();
   }
 }
