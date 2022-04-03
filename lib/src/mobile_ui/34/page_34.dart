861cec96a7e76938c3df802d39fd97d23cd8676c
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2022-04-04 00:46:25 +0100

    Bootstrapping page 34

---
+++ lib/src/mobile_ui/34/page_34.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Page34 extends StatefulWidget {
+  const Page34({Key? key}) : super(key: key);
+
+  @override
+  State<Page34> createState() => _Page34State();
+}
+
+class _Page34State extends State<Page34> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
