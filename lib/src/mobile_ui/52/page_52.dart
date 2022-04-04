d3389ca49e6f9093ad035d499de135781cfdb329
bukunmialuko <oluwabukunmi.aluko@gmail.com>
2022-04-04 15:38:49 +0100

    Page 52 fixed

--- lib/src/mobile_ui/52/page_52.dart
+++ lib/src/mobile_ui/52/page_52.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -109,82 +111,90 @@ class _Page52State extends State<Page52> {
                 ),
               ),
               Spacer(),
-              Container(
-                width: 343.w,
-                height: 190.h,
-                padding: EdgeInsets.only(
-                    left: 23.w, right: 30.w, top: 20.h, bottom: 15.h),
-                decoration: BoxDecoration(
-                  color: Color(0x66C4C4C4),
-                  borderRadius: BorderRadius.circular(32.r),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Model name",
-                      style: GoogleFonts.workSans(
-                        textStyle: TextStyle(
-                          fontSize: 24.sp,
-                          color: Colors.black,
-                          fontStyle: FontStyle.normal,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
+
+              /// ClipRect would also work here
+              ClipRRect(
+                borderRadius: BorderRadius.circular(30.r),
+                child: BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                  child: Container(
+                    width: 343.w,
+                    height: 190.h,
+                    padding: EdgeInsets.only(
+                        left: 23.w, right: 30.w, top: 20.h, bottom: 15.h),
+                    decoration: BoxDecoration(
+                      color: Color(0x66C4C4C4),
+                      borderRadius: BorderRadius.circular(32.r),
                     ),
-                    SizedBox(height: 10.h),
-                    Expanded(
-                      child: Text(
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed amet vel cursus at. Sit consectetur euismod dolor porttitor enim id tempus.",
-                        style: GoogleFonts.workSans(
-                          textStyle: TextStyle(
-                            fontSize: 14.sp,
-                            color: Colors.black,
-                            fontStyle: FontStyle.normal,
-                            fontWeight: FontWeight.w400,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Model name",
+                          style: GoogleFonts.workSans(
+                            textStyle: TextStyle(
+                              fontSize: 24.sp,
+                              color: Colors.black,
+                              fontStyle: FontStyle.normal,
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
                         ),
-                      ),
-                    ),
-                    SizedBox(height: 16.h),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: Container(
-                        width: 90.w,
-                        height: 30.h,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(
-                            30.h,
-                          ),
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "Follow",
-                              style: GoogleFonts.workSans(
-                                textStyle: TextStyle(
-                                  fontSize: 14.sp,
-                                  color: Colors.black,
-                                  fontStyle: FontStyle.normal,
-                                  fontWeight: FontWeight.w600,
-                                ),
+                        SizedBox(height: 10.h),
+                        Expanded(
+                          child: Text(
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed amet vel cursus at. Sit consectetur euismod dolor porttitor enim id tempus.",
+                            style: GoogleFonts.workSans(
+                              textStyle: TextStyle(
+                                fontSize: 14.sp,
+                                color: Colors.black,
+                                fontStyle: FontStyle.normal,
+                                fontWeight: FontWeight.w400,
                               ),
                             ),
-                            SizedBox(
-                              width: 4.w,
-                            ),
-                            SvgPicture.asset(
-                              "assets/svg/52/add_circle.svg",
-                              width: 24.w,
-                              height: 24.w,
-                            ),
-                          ],
+                          ),
                         ),
-                      ),
-                    )
-                  ],
+                        SizedBox(height: 16.h),
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: Container(
+                            width: 90.w,
+                            height: 30.h,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(
+                                30.h,
+                              ),
+                            ),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "Follow",
+                                  style: GoogleFonts.workSans(
+                                    textStyle: TextStyle(
+                                      fontSize: 14.sp,
+                                      color: Colors.black,
+                                      fontStyle: FontStyle.normal,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(
+                                  width: 4.w,
+                                ),
+                                SvgPicture.asset(
+                                  "assets/svg/52/add_circle.svg",
+                                  width: 24.w,
+                                  height: 24.w,
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
               ),
               SizedBox(
