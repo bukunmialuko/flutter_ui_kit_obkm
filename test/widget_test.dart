f6d39b2a4c319a59fb3a7d915c625d6048873d76
Bukunmi Aluko <oluwabukunmi.aluko@gmail.com>
2024-06-19 21:24:36 +0100

    chore: add sample widget tests (#93)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,23 +5,23 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_ui_kit_obkm/main.dart' as test_app;
+import 'package:flutter_ui_kit_obkm/src/screens/home/widgets/page_button_widget.dart';
 
 void main() {
-  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-  //   // Build our app and trigger a frame.
-  //   await tester.pumpWidget(const MyApp());
-  //
-  //   // Verify that our counter starts at 0.
-  //   expect(find.text('0'), findsOneWidget);
-  //   expect(find.text('1'), findsNothing);
-  //
-  //   // Tap the '+' icon and trigger a frame.
-  //   await tester.tap(find.byIcon(Icons.add));
-  //   await tester.pump();
-  //
-  //   // Verify that our counter has incremented.
-  //   expect(find.text('0'), findsNothing);
-  //   expect(find.text('1'), findsOneWidget);
-  // });
+  testWidgets('Test Homepage', (tester) async {
+    await tester.binding.setSurfaceSize(Size(390, 844));
+    await tester.pumpWidget(
+      test_app.App(),
+    );
+    await tester.pumpAndSettle();
+    expect(find.text('Flutter Ui Kit'), findsOneWidget);
+    final scrollableFinder = find.byType(Scrollable);
+    final textFinder = find.widgetWithText(PageButtonWidget, '100');
+    await tester.scrollUntilVisible(textFinder, 500,
+        scrollable: scrollableFinder);
+    expect(find.text('100'), findsOneWidget);
+  });
 }
