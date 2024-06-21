d911c3577b2a738f4318c6b209e554e54e358a35
b00lit3 <b00lit3@outlook.com>
2024-06-21 10:17:15 +0100

    add test utils

---
+++ test/util.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_ui_kit_obkm/main.dart' as test_app;
+import 'package:flutter_ui_kit_obkm/src/screens/home/widgets/page_button_widget.dart';
+
+Future<void> loadAppAndNavigate(WidgetTester tester, int page) async {
+  await tester.binding.setSurfaceSize(Size(390, 844));
+  await tester.pumpWidget(
+    test_app.App(),
+  );
+  await tester.pumpAndSettle();
+
+  expect(find.text('Flutter Ui Kit'), findsOneWidget);
+
+  final buttonFinder = find.widgetWithText(PageButtonWidget, '$page');
+  final scrollableFinder = find.byType(Scrollable);
+
+  await tester.scrollUntilVisible(
+    buttonFinder,
+    500,
+    scrollable: scrollableFinder,
+  );
+
+  await tester.tap(buttonFinder);
+  await tester.pumpAndSettle();
+
+  expect(find.text('Flutter Ui Kit'), findsNothing);
+}
